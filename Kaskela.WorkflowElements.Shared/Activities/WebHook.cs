94cd47a3cedc2ab95c1dfd88ab33d54f1442b39a
Michael Ochs <mike@mikefactorial.com>
2019-08-18 08:14:09 -0400

    Added javascript for calling custom action with webhook

---
+++ Kaskela.WorkflowElements.Shared/Activities/WebHook.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaskela.WorkflowElements.Shared.Activities
+{
+    public class EntitySpecification
+    {
+        public string EntitySchemaName { get; set; }
+        public Guid EntityId { get; set; }
+    }
+    public class WebHook : ContributingClasses.WorkflowBase
+    {
+        [Input("RequestHeaders")]
+        public InArgument<string> RequestHeaders { get; set; }
+
+        [RequiredArgument]
+        [Input("Request Url")]
+        public InArgument<string> RequestUrl { get; set; }
+
+        [Input("Request Body")]
+        public InArgument<string> RequestBody { get; set; }
+
+        public const int GET = 100000000;
+        public const int POST = 100000001;
+        public const int PUT = 100000002;
+        public const int PATCH = 100000003;
+        public const int DELETE = 100000004;
+
+        [RequiredArgument]
+        [Input("Request Method")]
+        [AttributeTarget("afk_workflowelementoption", "new_httprequestmethod")]
+        public InArgument<OptionSetValue> RequestMethod { get; set; }
+
+        [Output("Response Headers")]
+        public OutArgument<string> ResponseHeaders { get; set; }
+
+        [Output("Response Body")]
+        public OutArgument<string> ResponseBody { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string body = string.Empty;
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+            if (workflowContext != null && workflowContext.InputParameters.Contains("Target") && workflowContext.InputParameters["Target"] is Entity && string.IsNullOrEmpty(RequestBody.Get<string>(context)))
+            {
+                var entity = (Entity)workflowContext.InputParameters["Target"];
+                body = Stringify(new EntitySpecification() { EntityId = entity.Id });
+            }
+            else
+            {
+                body = RequestBody.Get<string>(context);
+            }
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(body, Encoding.UTF8, "application/json");
+
+                System.Threading.Tasks.Task<HttpResponseMessage> response = null;
+                if (this.RequestMethod != null)
+                {
+                    OptionSetValue value = this.RequestMethod.Get<OptionSetValue>(context);
+                    if (value != null && value.Value != 0)
+                    {
+                        switch (value.Value)
+                        {
+                            case GET:
+                                response = client.GetAsync(RequestUrl.Get<string>(context));
+                                break;
+                            case PATCH:
+                                response = client.PatchAsync(RequestUrl.Get<string>(context), content);
+                                break;
+                            case PUT:
+                                response = client.PutAsync(RequestUrl.Get<string>(context), content);
+                                break;
+                            case DELETE:
+                                response = client.DeleteAsync(RequestUrl.Get<string>(context));
+                                break;
+                            default:
+                                response = client.PostAsync(RequestUrl.Get<string>(context), content);
+                                break;
+
+                        }
+                    }
+                }
+                if (response != null)
+                {
+                    StringBuilder delimitedHeaders = new StringBuilder();
+                    foreach (var header in response.Result.Headers)
+                    {
+                        if (delimitedHeaders.Length > 0)
+                        {
+                            delimitedHeaders.Append(";");
+                        }
+                        delimitedHeaders.Append($"{header.Key}:{header.Value}");
+                    }
+                    ResponseHeaders.Set(context, delimitedHeaders.ToString());
+                    var responseString = response.Result.Content.ReadAsStringAsync();
+                    ResponseBody.Set(context, responseString.Result);
+                }
+            }
+        }
+        public static string Stringify(EntitySpecification obj)
+        {
+            // Create a stream to serialize the object to.  
+            var ms = new MemoryStream();
+
+            // Serializer the User object to the stream.  
+            var ser = new DataContractJsonSerializer(obj.GetType());
+            ser.WriteObject(ms, obj);
+            byte[] json = ms.ToArray();
+            ms.Close();
+            return Encoding.UTF8.GetString(json, 0, json.Length);
+        }
+
+    }
+
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUrl, HttpContent iContent)
+        {
+            Uri requestUri = new Uri(requestUrl);
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = iContent
+            };
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            return await client.SendAsync(request);
+        }
+    }
+}
---
+++ Kaskela.WorkflowElements.Shared/Scripts/_afk/common.js
@@ -0,0 +1,44 @@
+﻿function callWebHookAction(actionName, requestUrl, reloadAfterExecute) {
+        var serverURL = Xrm.Page.context.getClientUrl();
+        var req = new XMLHttpRequest();
+        var objectId = '';
+        if (Xrm.Page != null && Xrm.Page.data != null && Xrm.Page.data.entity != null) {
+            objectId = Xrm.Page.data.entity.getId().replace("{", "").replace("}", "");
+        }
+
+        var data = {
+            "Request": window.JSON.stringify({
+                "EntityId": objectId
+            }),
+            "RequestUrl": requestUrl
+        }
+
+        // specify name of the entity, record id and name of the action in the Wen API Url
+        req.open("POST", serverURL + "/api/data/v9.0/" + actionName, true);
+        req.setRequestHeader("Accept", "application/json");
+        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        req.setRequestHeader("OData-MaxVersion", "4.0");
+        req.setRequestHeader("OData-Version", "4.0");
+        req.onreadystatechange = function () {
+            if (this.readyState == 4 /* complete */) {
+                req.onreadystatechange = null;
+                if (this.status == 200 && this.response != null) {
+                    var responseObject = JSON.parse(this.response);
+                    console.trace(this.response);
+                    if (reloadAfterExecute) {
+                        Xrm.Utility.openEntityForm(Xrm.Page.data.entity.getEntityName(), Xrm.Page.data.entity.getId());
+                    }
+                    //callback(responseObject);
+                }
+                else {
+                    if (this.response != null && this.response != "") {
+                        var error = JSON.parse(this.response).error;
+                        console.trace(error.message);
+                        //errHandler(error.message);
+                    }
+                }
+            }
+        };
+        // send the request with the data for the input parameter
+        req.send(window.JSON.stringify(data));
+    }

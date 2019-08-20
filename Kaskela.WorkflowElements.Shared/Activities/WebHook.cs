b57994dbfcc55358434ed40af1b9e404e88e1f3e
Michael Ochs <mike@mikefactorial.com>
2019-08-19 22:03:35 -0400

    Changes for updated solutions

--- Kaskela.WorkflowElements.Shared/Activities/WebHook.cs
+++ Kaskela.WorkflowElements.Shared/Activities/WebHook.cs
@@ -16,7 +16,7 @@ namespace Kaskela.WorkflowElements.Shared.Activities
 {
     public class EntitySpecification
     {
-        public string EntitySchemaName { get; set; }
+        public string EntityLogicalName { get; set; }
         public Guid EntityId { get; set; }
     }
     public class WebHook : ContributingClasses.WorkflowBase
@@ -31,15 +31,26 @@ namespace Kaskela.WorkflowElements.Shared.Activities
         [Input("Request Body")]
         public InArgument<string> RequestBody { get; set; }
 
-        public const int GET = 100000000;
-        public const int POST = 100000001;
-        public const int PUT = 100000002;
-        public const int PATCH = 100000003;
-        public const int DELETE = 100000004;
+        public const int GET = 222540000;
+        public const int HEAD = 222540001;
+        public const int POST = 222540002;
+        public const int PUT = 222540003;
+        public const int DELETE = 222540004;
+        public const int TRACE = 222540005;
+        public const int OPTIONS = 222540006;
+        public const int CONNECT = 222540007;
+        public const int PATCH = 222540008;
+
+        public const int ASYNC = 222540001;
+
+        [RequiredArgument]
+        [Input("Synchronous Mode")]
+        [AttributeTarget("afk_workflowelementoption", "afk_synchronousmode")]
+        public InArgument<OptionSetValue> SynchronousMode { get; set; }
 
         [RequiredArgument]
         [Input("Request Method")]
-        [AttributeTarget("afk_workflowelementoption", "new_httprequestmethod")]
+        [AttributeTarget("afk_workflowelementoption", "afk_httpverb")]
         public InArgument<OptionSetValue> RequestMethod { get; set; }
 
         [Output("Response Headers")]
@@ -55,7 +66,7 @@ namespace Kaskela.WorkflowElements.Shared.Activities
             if (workflowContext != null && workflowContext.InputParameters.Contains("Target") && workflowContext.InputParameters["Target"] is Entity && string.IsNullOrEmpty(RequestBody.Get<string>(context)))
             {
                 var entity = (Entity)workflowContext.InputParameters["Target"];
-                body = Stringify(new EntitySpecification() { EntityId = entity.Id });
+                body = Stringify(new EntitySpecification() { EntityLogicalName = entity.LogicalName, EntityId = entity.Id });
             }
             else
             {
@@ -71,25 +82,7 @@ namespace Kaskela.WorkflowElements.Shared.Activities
                     OptionSetValue value = this.RequestMethod.Get<OptionSetValue>(context);
                     if (value != null && value.Value != 0)
                     {
-                        switch (value.Value)
-                        {
-                            case GET:
-                                response = client.GetAsync(RequestUrl.Get<string>(context));
-                                break;
-                            case PATCH:
-                                response = client.PatchAsync(RequestUrl.Get<string>(context), content);
-                                break;
-                            case PUT:
-                                response = client.PutAsync(RequestUrl.Get<string>(context), content);
-                                break;
-                            case DELETE:
-                                response = client.DeleteAsync(RequestUrl.Get<string>(context));
-                                break;
-                            default:
-                                response = client.PostAsync(RequestUrl.Get<string>(context), content);
-                                break;
-
-                        }
+                        response = SendRequest(context, client, value.Value, content);
                     }
                 }
                 if (response != null)
@@ -106,6 +99,48 @@ namespace Kaskela.WorkflowElements.Shared.Activities
                     ResponseHeaders.Set(context, delimitedHeaders.ToString());
                     var responseString = response.Result.Content.ReadAsStringAsync();
                     ResponseBody.Set(context, responseString.Result);
+                }
+            }
+        }
+        public async Task<HttpResponseMessage> SendRequest(CodeActivityContext context, HttpClient client, int method, StringContent content)
+        {
+            OptionSetValue syncMode = this.SynchronousMode.Get<OptionSetValue>(context);
+            if (syncMode != null && syncMode.Value == ASYNC)
+            {
+                switch (method)
+                {
+                    case GET:
+                        client.GetAsync(RequestUrl.Get<string>(context));
+                        return null;
+                    case PATCH:
+                        client.PatchAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                    case PUT:
+                        client.PutAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                    case DELETE:
+                        client.DeleteAsync(RequestUrl.Get<string>(context));
+                        return null;
+                    default:
+                        client.PostAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                }
+
+            }
+            else
+            {
+                switch (method)
+                {
+                    case GET:
+                        return await client.GetAsync(RequestUrl.Get<string>(context));
+                    case PATCH:
+                        return await client.PatchAsync(RequestUrl.Get<string>(context), content);
+                    case PUT:
+                        return await client.PutAsync(RequestUrl.Get<string>(context), content);
+                    case DELETE:
+                        return await client.DeleteAsync(RequestUrl.Get<string>(context));
+                    default:
+                        return await client.PostAsync(RequestUrl.Get<string>(context), content);
                 }
             }
         }

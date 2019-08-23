a31f10fd87f418e8708d238ee6bb1b29f464268b
Mike! <42348035+mikefactorial@users.noreply.github.com>
2019-08-23 15:37:33 -0400

    Added Webhook Functionality (#1)
    * Added WebHook Custom Activity
    
    * Added javascript for calling custom action with webhook
    
    * Changes for updated solutions
    
    * Added Newtonsoft source to shared project to handle JsonPaths
    
    * Removed some unused files
    
    * Update README.md
    
    * Updated Webhook to send full entity json
    
    * WebHook Update
    
    * Added activity to serialize an entity
    
    * Updated Paramenter to JsonPath
    
    * Update README.md
    
    * Removed some unused code from GetValueFromJsonString
    
    * Removed key files from projects
    
    * Removed extraneous js file
    
    * Removing Key Files from Solution Root
    
    * Update to throw exception if Webhook call is not successful

---
+++ Kaskela.WorkflowElements.Shared/Activities/GetValueFromJsonString.cs
@@ -0,0 +1,101 @@
+﻿using Kaskela.WorkflowElements.Shared.ContributingClasses;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaskela.WorkflowElements.Shared.Activities
+{
+    public class GetValueFromJson : ContributingClasses.WorkflowBase
+    {
+        [RequiredArgument]
+        [Input("Json String")]
+        public InArgument<string> JsonString { get; set; }
+
+        [RequiredArgument]
+        [Input("Json Path")]
+        public InArgument<string> JsonPath { get; set; }
+
+        [Output("Single Value - Result as Whole Number")]
+        public OutArgument<int> Result_WholeNumber { get; set; }
+
+        [Output("Single Value - Result as DateTime")]
+        public OutArgument<DateTime> Result_DateTime { get; set; }
+
+        [Output("Single Value - Result as Decimal")]
+        public OutArgument<decimal> Result_Decimal { get; set; }
+
+        [Output("Single Value - Result as Double")]
+        public OutArgument<double> Result_Double { get; set; }
+
+        [Output("Single Value - Result as Money")]
+        public OutArgument<Money> Result_Money { get; set; }
+
+        [Output("Single Value - Result as ID")]
+        public OutArgument<string> Result_Guid { get; set; }
+
+        [Output("Single Value - Result as Text")]
+        public OutArgument<string> Result_Text { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+            if (!string.IsNullOrEmpty(JsonString.Get<string>(context)) && !string.IsNullOrEmpty(JsonPath.Get<string>(context)))
+            {
+                JObject jsonObject = JObject.Parse(JsonString.Get<string>(context));
+                JToken token = jsonObject.SelectToken(JsonPath.Get<string>(context));
+                if(token != null)
+                {
+                    string value = (string)token;
+                    Result_Text.Set(context, value);
+
+                    Guid id = new Guid();
+                    if (Guid.TryParse(value, out id))
+                    {
+                        this.Result_Guid.Set(context, value);
+                    }
+
+                    DateTime dateTime = new DateTime();
+                    if (DateTime.TryParse(value, out dateTime))
+                    {
+                        this.Result_DateTime.Set(context, dateTime);
+                    }
+
+                    decimal decimalValue = new decimal();
+                    if (decimal.TryParse(value, out decimalValue))
+                    {
+                        this.Result_Decimal.Set(context, decimalValue);
+                        try
+                        {
+                            this.Result_Money.Set(context, new Money(decimalValue));
+                        }
+                        catch { }
+                    }
+
+                    double doubleValue = new double();
+                    if (double.TryParse(value, out doubleValue))
+                    {
+                        this.Result_Double.Set(context, doubleValue);
+                    }
+
+                    int intValue = 0;
+                    if (int.TryParse(value, out intValue))
+                    {
+                        this.Result_WholeNumber.Set(context, intValue);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kaskela.WorkflowElements.Shared/Activities/WebHook.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using Newtonsoft.Json;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaskela.WorkflowElements.Shared.Activities
+{
+    public class EntitySpecification
+    {
+        public string EntityLogicalName { get; set; }
+        public Guid EntityId { get; set; }
+    }
+    public class WebHook : ContributingClasses.WorkflowBase
+    {
+        [Input("RequestHeaders")]
+        public InArgument<string> RequestHeaders { get; set; }
+
+        [RequiredArgument]
+        [Input("Request Url")]
+        public InArgument<string> RequestUrl { get; set; }
+
+        [Input("Request Body")]
+        public InArgument<string> RequestBody { get; set; }
+
+
+        public const int GET = 222540000;
+        public const int HEAD = 222540001;
+        public const int POST = 222540002;
+        public const int PUT = 222540003;
+        public const int DELETE = 222540004;
+        public const int TRACE = 222540005;
+        public const int OPTIONS = 222540006;
+        public const int CONNECT = 222540007;
+        public const int PATCH = 222540008;
+
+        public const int ASYNC = 222540001;
+
+        [RequiredArgument]
+        [Input("Synchronous Mode")]
+        [AttributeTarget("afk_workflowelementoption", "afk_synchronousmode")]
+        public InArgument<OptionSetValue> SynchronousMode { get; set; }
+
+        [RequiredArgument]
+        [Input("Request Method")]
+        [AttributeTarget("afk_workflowelementoption", "afk_httpverb")]
+        public InArgument<OptionSetValue> RequestMethod { get; set; }
+
+        [Output("Response Headers")]
+        public OutArgument<string> ResponseHeaders { get; set; }
+
+        [Output("Response Body")]
+        public OutArgument<string> ResponseBody { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string body = string.Empty;
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+
+            if(!string.IsNullOrEmpty(RequestBody.Get<string>(context)))
+            {
+                body = RequestBody.Get<string>(context);
+            }
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(body, Encoding.UTF8, "application/json");
+
+                System.Threading.Tasks.Task<HttpResponseMessage> response = null;
+                if (this.RequestMethod != null)
+                {
+                    OptionSetValue value = this.RequestMethod.Get<OptionSetValue>(context);
+                    if (value != null && value.Value != 0)
+                    {
+                        response = SendRequest(context, client, value.Value, content);
+                    }
+                }
+                if (response != null)
+                {
+                    response.Result.EnsureSuccessStatusCode();
+                    StringBuilder delimitedHeaders = new StringBuilder();
+                    foreach (var header in response.Result.Headers)
+                    {
+                        if (delimitedHeaders.Length > 0)
+                        {
+                            delimitedHeaders.Append(";");
+                        }
+                        delimitedHeaders.Append($"{header.Key}:{header.Value}");
+                    }
+                    ResponseHeaders.Set(context, delimitedHeaders.ToString());
+                    var responseString = response.Result.Content.ReadAsStringAsync();
+                    ResponseBody.Set(context, responseString.Result);
+                }
+            }
+        }
+        public async Task<HttpResponseMessage> SendRequest(CodeActivityContext context, HttpClient client, int method, StringContent content)
+        {
+            OptionSetValue syncMode = this.SynchronousMode.Get<OptionSetValue>(context);
+            if (syncMode != null && syncMode.Value == ASYNC)
+            {
+                switch (method)
+                {
+                    case GET:
+                        client.GetAsync(RequestUrl.Get<string>(context));
+                        return null;
+                    case PATCH:
+                        client.PatchAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                    case PUT:
+                        client.PutAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                    case DELETE:
+                        client.DeleteAsync(RequestUrl.Get<string>(context));
+                        return null;
+                    case POST:
+                        client.PostAsync(RequestUrl.Get<string>(context), content);
+                        return null;
+                    default:
+                        throw new InvalidPluginExecutionException("The Request Method supplied is not supported. Try using GET, PATCH, PUT, DELETE or POST");
+                }
+            }
+            else
+            {
+                switch (method)
+                {
+                    case GET:
+                        return await client.GetAsync(RequestUrl.Get<string>(context));
+                    case PATCH:
+                        return await client.PatchAsync(RequestUrl.Get<string>(context), content);
+                    case PUT:
+                        return await client.PutAsync(RequestUrl.Get<string>(context), content);
+                    case DELETE:
+                        return await client.DeleteAsync(RequestUrl.Get<string>(context));
+                    case POST:
+                        return await client.PostAsync(RequestUrl.Get<string>(context), content);
+                    default:
+                        throw new InvalidPluginExecutionException("The Request Method supplied is not supported. Try using GET, PATCH, PUT, DELETE or POST");
+                }
+            }
+        }
+    }
+
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUrl, HttpContent iContent)
+        {
+            Uri requestUri = new Uri(requestUrl);
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = iContent
+            };
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            return await client.SendAsync(request);
+        }
+    }
+}
